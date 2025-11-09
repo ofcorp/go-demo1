d5717eb8bee744b11751b66a5be43d0a0db04168
ofcorp <director@demrev.biz>
2025-11-10 01:15:47 +0300

    1-converter dz 1 - 1

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+const USDToEURRate = 0.85
+const USDToRURRate = 80.0
+
+func main() {
+	amountInEUR := 100.0
+	EURtoRUR := amountInEUR / USDToEURRate * USDToRURRate
+	fmt.Printf("%.2f EUR равно %.2f RUR\n", amountInEUR, EURtoRUR)
 }
