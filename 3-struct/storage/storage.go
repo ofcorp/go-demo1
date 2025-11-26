576ab71404119fb17f0ffa193ef2730d1acb0520
ofcorp <director@demrev.biz>
2025-11-26 23:33:11 +0300

    3-struct dz 4

--- 3-struct/storage/storage.go
+++ 3-struct/storage/storage.go
@@ -1,1 +1,35 @@
 package storage
+
+import (
+	"encoding/json"
+	"go-demo1/3-struct/file"
+	"time"
+)
+
+type Bin struct {
+	Id        string    `json:"id"`
+	Private   bool      `json:"private"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+}
+
+var BinList = []Bin{}
+
+func SaveBins(binList []Bin) error {
+	data, err := json.Marshal(binList)
+	if err != nil {
+		return err
+	}
+	const outPath = "bins.json"
+	err = file.WriteFile(data, outPath)
+	return err
+}
+
+func LoadBins(name string) error {
+	data, err := file.ReadFile(name)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(data, &BinList)
+	return err
+}
